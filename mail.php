81810c8f4ac964863d3e60f8feaad0443a197b5b
Sugandh Kumar <sugandhkumar9@gmail.com>
2022-05-01 23:01:46 +0530

    email changed

--- mail.php
+++ mail.php
@@ -41,7 +41,7 @@
     
         //Recipients
         $mail->setFrom('info@mantratechro.com', 'Mantratech');
-        $mail->addAddress("sugandhkumar9@gmail.com", "Mantratech");     // Add a recipient
+        $mail->addAddress("mantratechro@gmail.com", "Mantratech");     // Add a recipient
         $mail->addReplyTo($email, $name);
     
         // Attachments
